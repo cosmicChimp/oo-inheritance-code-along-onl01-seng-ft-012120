200411fac71329193e391bee61bc0c7f988581de
Ian Marrs <ian.t.marrs@gmail.com>
2020-02-06 17:35:53 +0000

    Automatically backed up by Learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,4 +1,5 @@
 class Vehicle
+  
   attr_accessor :wheel_size, :wheel_number
   
   def initialize(wheel_size, wheel_number)
@@ -7,9 +8,11 @@ class Vehicle
   end
   
   def go 
-    "vrrrrrrr"
-
-
-
+    "vrrrrrrrooom!"
+  end
+  
+  def fill_up_tank
+    "filling up!"
+  end
 
 end
